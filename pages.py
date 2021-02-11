a28f236ae40a840fc767136960c38f9a3fc6e72f
S0nic014 <47799401+S0nic014@users.noreply.github.com>
2021-02-11 05:03:15 +0000

    Add unreal project path to others page

--- pages.py
+++ pages.py
@@ -1,9 +1,7 @@
 from PySide2 import QtWidgets
+import luna
 from luna import Logger
 from luna import Config
-from luna import LunaVars
-from luna import TestVars
-from luna import HudVars
 from luna.interface import shared_widgets
 from luna.interface import hud
 reload(shared_widgets)
@@ -79,35 +77,35 @@ class DeveloperPage(PageWidget):
         config_dict = Config.load()
 
         # Logging
-        Logger.set_level(config_dict.get(LunaVars.logging_level))
+        Logger.set_level(config_dict.get(luna.LunaVars.logging_level))
         self.logging_level_field.setCurrentText(Logger.get_level(name=1))
 
         # Testing
-        self.testing_temp_dir.line_edit.setText(config_dict.get(TestVars.temp_dir))
-        self.testing_buffer_output_cb.setChecked(config_dict.get(TestVars.buffer_output))
-        self.testing_new_file_cb.setChecked(config_dict.get(TestVars.new_file))
-        self.testing_delete_files_cb.setChecked(config_dict.get(TestVars.delete_files))
-        self.testing_delete_dirs_cb.setChecked(config_dict.get(TestVars.delete_dirs))
+        self.testing_temp_dir.line_edit.setText(config_dict.get(luna.TestVars.temp_dir))
+        self.testing_buffer_output_cb.setChecked(config_dict.get(luna.TestVars.buffer_output))
+        self.testing_new_file_cb.setChecked(config_dict.get(luna.TestVars.new_file))
+        self.testing_delete_files_cb.setChecked(config_dict.get(luna.TestVars.delete_files))
+        self.testing_delete_dirs_cb.setChecked(config_dict.get(luna.TestVars.delete_dirs))
 
         # Misc
-        self.misc_pyport_field.setValue(config_dict.get(LunaVars.command_port))
+        self.misc_pyport_field.setValue(config_dict.get(luna.LunaVars.command_port))
 
     def save_config(self):
         Logger.debug("Developer page - saving config...")
         new_config = {}
         # Logging
         Logger.set_level(self.logging_level_field.currentText())
-        new_config[LunaVars.logging_level] = Logger.get_level()
+        new_config[luna.LunaVars.logging_level] = Logger.get_level()
 
         # Testing
-        new_config[TestVars.temp_dir] = self.testing_temp_dir.line_edit.text()
-        new_config[TestVars.buffer_output] = self.testing_buffer_output_cb.isChecked()
-        new_config[TestVars.new_file] = self.testing_new_file_cb.isChecked()
-        new_config[TestVars.delete_files] = self.testing_delete_files_cb.isChecked()
-        new_config[TestVars.delete_dirs] = self.testing_delete_dirs_cb.isChecked()
+        new_config[luna.TestVars.temp_dir] = self.testing_temp_dir.line_edit.text()
+        new_config[luna.TestVars.buffer_output] = self.testing_buffer_output_cb.isChecked()
+        new_config[luna.TestVars.new_file] = self.testing_new_file_cb.isChecked()
+        new_config[luna.TestVars.delete_files] = self.testing_delete_files_cb.isChecked()
+        new_config[luna.TestVars.delete_dirs] = self.testing_delete_dirs_cb.isChecked()
 
         # Misc
-        new_config[LunaVars.command_port] = self.misc_pyport_field.value()
+        new_config[luna.LunaVars.command_port] = self.misc_pyport_field.value()
 
         # Update config
         Config.update(new_config)
@@ -123,11 +121,15 @@ class OtherPage(PageWidget):
         self._create_connections()
 
     def _create_widgets(self):
+        # HUD
         self.hud_grp = QtWidgets.QGroupBox("HUD")
         self.hud_section_field = QtWidgets.QSpinBox()
         self.hud_block_field = QtWidgets.QSpinBox()
         self.hud_section_field.setMinimum(0)
         self.hud_section_field.setMaximum(9)
+        # Unreal
+        self.unreal_grp = QtWidgets.QGroupBox("Unreal Engine")
+        self.unreal_project = shared_widgets.PathWidget(label_text="Project path:", dialog_label="Set Unreal project")
 
     def _create_layous(self):
         hud_layout = QtWidgets.QFormLayout()
@@ -135,8 +137,14 @@ class OtherPage(PageWidget):
         hud_layout.addRow("Section:", self.hud_section_field)
         self.hud_grp.setLayout(hud_layout)
 
-        self.main_layout = QtWidgets.QHBoxLayout()
+        unreal_layout = QtWidgets.QVBoxLayout()
+        unreal_layout.addWidget(self.unreal_project)
+        self.unreal_grp.setLayout(unreal_layout)
+
+        self.main_layout = QtWidgets.QVBoxLayout()
         self.main_layout.addWidget(self.hud_grp)
+        self.main_layout.addWidget(self.unreal_grp)
+        self.main_layout.addStretch()
         self.setLayout(self.main_layout)
 
     def _create_connections(self):
@@ -146,16 +154,18 @@ class OtherPage(PageWidget):
         Logger.debug("Other page - loading config...")
         config_dict = Config.load()
         # HUD
-        self.hud_section_field.setValue(config_dict.get(HudVars.section))
-        self.hud_block_field.setValue(config_dict.get(HudVars.block))
+        self.hud_section_field.setValue(config_dict.get(luna.HudVars.section))
+        self.hud_block_field.setValue(config_dict.get(luna.HudVars.block))
+        self.unreal_project.line_edit.setText(config_dict.get(luna.UnrealVars.project, ""))
 
     def save_config(self):
         Logger.debug("Other page - saving config...")
         new_config = {}
 
         # HUD
-        new_config[HudVars.section] = self.hud_section_field.value()
-        new_config[HudVars.block] = self.hud_block_field.value()
+        new_config[luna.HudVars.section] = self.hud_section_field.value()
+        new_config[luna.HudVars.block] = self.hud_block_field.value()
+        new_config[luna.UnrealVars.project] = self.unreal_project.line_edit.text()
 
         Config.update(new_config)
         Logger.debug("Other page - saved config: {0}".format(new_config))
